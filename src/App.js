c765715a88d1233d31d1a905b6f6d0eaaf2a3fba
Awasarmol Nikita <me@here.com>
2022-09-18 23:47:41 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{backgroundColor: '#000'}}></Box>
+  </BrowserRouter>
+)
+
+
+export default App;
