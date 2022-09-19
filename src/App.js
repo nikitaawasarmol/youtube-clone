2acdad479ab71502e2dfe7187cbd3b056c3965dd
Awasarmol Nikita <nikitaawasarmol@gmail.com>
2022-09-19 15:28:38 +0530

    added search bar and feed

--- src/App.js
+++ src/App.js
@@ -7,14 +7,15 @@ import ChannelDetail from './components/ChannelDetail';
 import SearchFeed from './components/SearchFeed';
 const App = () => (
   <BrowserRouter>
-    <Box sx={{backgroundColor: '#000'}}></Box>
+    <Box sx={{backgroundColor: '#000'}}>
     <Navbar />
     <Routes>
-      <Route path="/" exact element={<Feed />}/>
+      <Route exact path="/"  element={<Feed />}/>
       <Route path="/video/:id" element={<VideoDetail />}/>
       <Route path="/channel/:id" element={<ChannelDetail />}/>
       <Route path="/search/:searchTerm" element={<SearchFeed />}/> 
     </Routes>
+    </Box>
   </BrowserRouter>
 )
 
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" }}}>
+      <Box sx={{ height: { sx: "auto", md: "92vh" } ,borderRight: "1px solid #3d3d3d", px: {sx:0, md:2 }}}>
+        
+      <Typography className='copyright' varient="body2" sx={{mt: 1.5, color: '#000'}}>
+        Copyright 2022 Nikita Awasarmol
+      </Typography>
+
+      </Box>
+      {/* Siedbar */}
+    </Stack>
   )
 }
 
