c5debe91a3d5a585b6aacfc624053be71e8928b8
Awasarmol Nikita <nikitaawasarmol@gmail.com>
2022-09-20 21:21:34 +0530

    added sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-// import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
+import Sidebar from './Sidebar';
 
 const Feed = () => {
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" }}}>
-      <Box sx={{ height: { sx: "auto", md: "92vh" } ,borderRight: "1px solid #3d3d3d", px: {sx:0, md:2 }}}>
-      
-      <Typography className='copyright' varient="body2" sx={{mt: 1.5, color: '#000'}}>
+      <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: "1px solid #3d3d3d", px: {sx:0, md:2 }}}>
+      <Sidebar />
+      <Typography className='copyright' varient="body2" sx={{mt: 1.5, color: '#fff'}}>
         Copyright 2022 Nikita Awasarmol
       </Typography>
 
