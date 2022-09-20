e0d974f9321d941cc9a52d107fb05e63307d5af2
Awasarmol Nikita <nikitaawasarmol@gmail.com>
2022-09-20 23:40:47 +0530

    fetched api

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,9 @@
 import { Stack,} from '@mui/material';
 import { categories } from '../utils/constants';
 
-const selectedCategory = "New"
 
-const Sidebar = () => (
+
+const Sidebar = ({selectedCategory, setSelectedCategory}) => (
     <Stack 
     direction="row"
     sx={{
@@ -13,6 +13,7 @@ const Sidebar = () => (
     }}>
         {categories.map((category) => (
             <button className='category-btn'
+            onClick={() => setSelectedCategory(category.name)}
             style={{
                 background: category.name === selectedCategory && '#FC1503',
                 color: 'white'
@@ -23,7 +24,7 @@ const Sidebar = () => (
                 <span style={{ opacity: category.name === selectedCategory ? '1' : '0.5'}}>{category.name}</span>
             </button>
         ))}
-        Sidebar</Stack>
+        </Stack>
   )
 
 
