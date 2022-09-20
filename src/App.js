15bbd37a26806bf5bcc51146faf722b5cd9b1ca7
Awasarmol Nikita <nikitaawasarmol@gmail.com>
2022-09-20 20:49:45 +0530

    added feed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Feed from './components/Feed';
 import VideoDetail from './components/VideoDetail';
 import ChannelDetail from './components/ChannelDetail';
 import SearchFeed from './components/SearchFeed';
+
 const App = () => (
   <BrowserRouter>
     <Box sx={{backgroundColor: '#000'}}>
