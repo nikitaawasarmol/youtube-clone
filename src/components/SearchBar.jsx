69553ac8144e106a81395788063e14d1a3ade6a0
Awasarmol Nikita <nikitaawasarmol@gmail.com>
2022-09-22 16:22:47 +0530

    implemented search feed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,10 +4,23 @@ import { Paper, IconButton } from '@mui/material';
 import { Search } from '@mui/icons-material';
 
 const SearchBar = () => {
-  return (
+    const [searchTerm, setsearchTerm] = useState('')
+    
+    const navigate = useNavigate();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+
+        if(searchTerm) {
+            navigate(`/search/${searchTerm}`);
+
+            setsearchTerm('')
+        }
+    }
+    return (
     <Paper 
         component="form"
-        onSubmit={() => {}}
+        onSubmit={handleSubmit}
         sx={{
             borderRadius: 20,
             border: '1px solid #e3e3e3',
@@ -19,8 +32,8 @@ const SearchBar = () => {
         <input
             className='search-bar'
             placeholder="Search..."
-            value=""
-            onChange={() => {}}
+            value={searchTerm}
+            onChange={(e) => {setsearchTerm(e.target.value)}}
         />
         <IconButton type="submit" sx={{
             p:'8px', color: 'red'
